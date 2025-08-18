eb1cd37cfad8338f1fe41dcd9d9f6d4c13eababc
kaustubhduse <202251045@iiitvadodara.ac.in>
2025-08-18 22:16:26 +0530

    Added res

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-import app from "./app.js";
+import express from "express";
+import cors from "cors";
+import chatRoutes from "./routes/chatRoutes.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+app.use(express.json());
+
+app.use("/api", chatRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Chat API 🚀");
+});
 
 const PORT = 3001;
-
 app.listen(PORT, () => {
   console.log(`🚀 Backend running at http://localhost:${PORT}`);
 });
